6dbf87696450ce398404683babee925c96160131
niklas9898 <niklas.rasch@gmx.de>
2023-03-28 12:24:40 +0200

    'remove couts'

--- source/cuda/cudaDipolarGPSolver.cu
+++ source/cuda/cudaDipolarGPSolver.cu
@@ -846,9 +846,6 @@ namespace UltraCold
                                 s = u * u + v * v;
                             } while (s >= 1.0 || s == 0);
 
-                            std::cout << "u" << u << std::endl;
-                            std::cout << "v" << v << std::endl;
-
                             s = sqrt((-2.0 * log(s)) / s);
                             u = u * s;
                             v = v * s;
